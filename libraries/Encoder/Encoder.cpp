e80d63cc6b1fc4e8199e8ff9e73032bc837bacc6
Blake Giles <bagiles97@gmail.com>
2019-02-28 18:44:16 -0600

    Working encoder file

--- libraries/Encoder/Encoder.cpp
+++ libraries/Encoder/Encoder.cpp
@@ -9,16 +9,21 @@
  */
 
 #include "Encoder.h"
-
+volatile int  Encoder::newVal = 0;
+volatile int Encoder::oldVal = 0;
+volatile int Encoder::outVal = 0;
+volatile bool Encoder::encoder_A_set = false;
 volatile bool Encoder::encoder_B_set = false;
 volatile int Encoder::encoder_ticks = START_TICKS;
+const int Encoder::matrixVal [16] = {0,-1,1,2,1,0,2,-1,-1,2,0,1,2,1,-1,0};
 
 void Encoder::init()
 {
     pinMode(PIN_A, INPUT_PULLUP);
     pinMode(PIN_B, INPUT_PULLUP);
 
-    attachInterrupt(ENCODER_INTERRUPT, Encoder::_tick, RISING);
+    attachInterrupt(digitalPinToInterrupt(PIN_A), Encoder::_tick, CHANGE);
+	attachInterrupt(digitalPinToInterrupt(PIN_B), Encoder::_tick, CHANGE);
 }
 
 int Encoder::getPosition()
@@ -28,12 +33,24 @@ int Encoder::getPosition()
 
 void Encoder::_tick()
 {
+	noInterrupts();
     encoder_B_set = digitalReadFast(PIN_B);
-
+	encoder_A_set = digitalReadFast(PIN_A);
     //increment if A leads B
-    #ifdef ENCODER_REVERSED
-        encoder_ticks -= (encoder_B_set) ? -1 : 1;
-    #else
-        encoder_ticks += (encoder_B_set) ? -1 : 1;
-    #endif
+	Encoder::oldVal = Encoder::newVal;
+	Encoder::newVal = encoder_A_set * 2 + encoder_B_set;   // Convert binary input to decimal value
+	Encoder::outVal = matrixVal [Encoder::oldVal * 4 + Encoder::newVal];
+	if(Encoder::outVal== 1){
+		encoder_ticks++;
+	}
+	else if(Encoder::outVal== -1){
+		encoder_ticks--;
+	}
+	else{
+	 		//do nothing
+	}
+	interrupts();
+	
+	
 }
+
--- libraries/Encoder/Encoder.h
+++ libraries/Encoder/Encoder.h
@@ -15,21 +15,27 @@
 #include <digitalWriteFast.h>
 
 #define ENCODER_INTERRUPT 4
-#define PIN_A 19
-#define PIN_B 25
+#define PIN_A 2
+#define PIN_B 3
 #define START_TICKS 256 //parallel to ground (1/4 of 1024)
 
 class Encoder {
     public:
-        Encoder(){}
+        Encoder(){
+		}
 
         void init();
         int getPosition();
 
     private:
+		static volatile bool encoder_A_set;
         static volatile bool encoder_B_set;
         static volatile int encoder_ticks;
-
+		static volatile int newVal;
+		static volatile int oldVal;
+		static volatile int outVal;
+		static const int matrixVal [16];
+	
         static void _tick();
 };
 
